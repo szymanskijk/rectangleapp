e21f7861de1c79f496cdfac9b121ee6a3dc994dd
Kuba <szymanski.jk1@gmail.com>
2020-03-24 23:32:07 +0100

    obliczanie obwodu dodane

---
+++ src/main/java/jk/szymanski/rectangleapp/FigureApi.java
@@ -0,0 +1,14 @@
+package jk.szymanski.rectangleapp;
+
+
+public class FigureApi {
+
+    public int calc2(){
+        System.out.println("ddu");
+        return 100;
+    }
+
+    public void calculate(int siteA, int siteB){
+        System.out.println("dupa");
+    }
+}
--- src/main/java/jk/szymanski/rectangleapp/RectangleGui.java
+++ src/main/java/jk/szymanski/rectangleapp/RectangleGui.java
@@ -1,6 +1,7 @@
 package jk.szymanski.rectangleapp;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.login.AbstractLogin;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
@@ -10,10 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class RectangleGui extends VerticalLayout {
 
     private RectangleRepo rectangleRepo;
-
+    private FigureApi figureApi;
     private TextField textFieldHeigth;
     private TextField textFieldWidth;
     private Button button;
+    private Button calculateButton;
 
     @Autowired
     public RectangleGui(RectangleRepo rectangleRepo) {
@@ -21,18 +23,25 @@ public class RectangleGui extends VerticalLayout {
         textFieldHeigth = new TextField("Podaj wysokość");
         textFieldWidth = new TextField("Podaj szerokość");
         button = new Button("Dodaj!");
+        calculateButton = new Button("Oblicz obwód!");
 
         button.addClickListener(clickEvent -> addRectangle());
-
+        calculateButton.addClickListener((clickEvent -> calculate(Integer.parseInt(textFieldWidth.getValue()),Integer.parseInt(textFieldHeigth.getValue()))));
         add(textFieldHeigth);
         add(textFieldWidth);
         add(button);
+        add(calculateButton);
     }
     public void addRectangle(){
         Rectangle rectangle = new Rectangle();
         rectangle.setHeight(Integer.parseInt(textFieldHeigth.getValue()));
         rectangle.setWidth(Integer.parseInt(textFieldWidth.getValue()));
         rectangleRepo.save(rectangle);
-
     }
+    public void calculate(int siteA, int siteB){
+        int result = siteA*2 + siteB*2;
+        TextField textAns = new TextField("Obwód wynosi: " + result);
+        add(textAns);
+    }
+
 }
